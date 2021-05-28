16a025de78eb7ed5f7eb02a87701471c54724c25
AndreaCristn <83725322+AndreaCristn@users.noreply.github.com>
2021-05-28 12:13:29 -0300

    Projeto 1

--- quinzena1/lista-logica/exercicios.js
+++ quinzena1/lista-logica/exercicios.js
@@ -64,7 +64,9 @@ function imprimeTresCoresFavoritas() {
 
 // Exercício 6
 function retornaStringEmMaiuscula() {
-  // escreva seu código aqui
+  const fraseM = prompt("Digite uma frase")
+  console.log(fraseM.toUpperCase())
+
 }
 
 // Exercício 7
@@ -77,25 +79,50 @@ function calculaIngressosEspetaculo() {
 
 // Exercício 8
 function checaStringsMesmoTamanho() {
-  // escreva seu código aqui
+  let palavra1 = prompt("Digite a primeira palavra")
+  let palavra2 = prompt("Digite a segunda palavra")
+  console.log(palavra1.length == palavra2.length)
 }
 
 // Exercício 9
 function checaIgualdadeDesconsiderandoCase() {
-  // escreva seu código aqui
+  let palavra_1 = prompt("Digite a primeira palavra")
+  let palavra_2 = prompt("Digite a segunda palavra")
+  console.log(palavra_1.toUpperCase() === palavra_2.toUpperCase())
 }
 
 // Exercício 10
 function checaRenovacaoRG() {
-  // escreva seu código aqui
+  const ano_atual = Number(prompt("Digite o ano atual"))
+  const ano_nasc = Number(prompt("Digite seu ano de nascimento"))
+  const ano_rg = Number(prompt("Digite o ano em que sua carteira foi emitida"))
+
+  const idade = ano_atual - ano_nasc
+  const idadeCarteira = ano_atual - ano_rg
+  const caso1 = idade <= 20 && idadeCarteira >= 5 
+  const caso2 = idade >20 && idade <= 50 && idadeCarteira >= 10 
+  const caso3 = idade >50 && idadeCarteira >= 15 
+  console.log(caso1 || caso2 || caso3)
+  
 }
 
 // Exercício 11
 function checaAnoBissexto() {
-  // escreva seu código aqui
+  const ano = Number(prompt("Digite o ano "))
+  const div400 = (ano % 400) == 0
+  const div4 = (ano % 4) == 0
+  const div100 = (ano % 100) == 0
+
+  const test1 = div400 // true ou false
+  const test2 = div4 && !(div100 && !div400)
+
+  console.log(test1 || test2)
 }
 
 // Exercício 12
 function checaValidadeInscricaoLabenu() {
-  // escreva seu código aqui
+ const Maior18 = prompt("Você tem mais de 18 anos?")
+ const enCompleto = prompt("Você possui ensino médio completo?")
+ const disponibilidade = prompt("Você possui disponibilidade exclusiva durante os horários do curso?")
+ console.log((Maior18 == "sim") && (enCompleto == "sim") && (disponibilidade == "sim"))
 }
